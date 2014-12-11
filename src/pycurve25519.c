6cb15d7610c921956d7b33435fdf362ef7bf2ca4
TomCrypto <thomas.beneteau@yahoo.fr>
2014-12-12 07:08:10 +1300

    Added experimental Python 3.x support.

--- src/pycurve25519.c
+++ src/pycurve25519.c
@@ -6,6 +6,97 @@ extern void curve25519_donna(      uint8_t *mypublic,
 
 static const uint8_t base_pt[32] = {9}; // x = 9
 
+#if PY_MAJOR_VERSION >= 3
+
+#include <string.h>
+
+// Usage: private = curve25519.genkey() -> generate private key
+static PyObject *curve25519_genkey(PyObject *self, PyObject *args)
+{
+    Py_buffer view;
+    uint8_t private_key[32];
+    PyObject *key_len = PyTuple_Pack(1, Py_BuildValue("i", 32));
+    PyObject *os = PyImport_ImportModule("os");
+    PyObject *urandom = PyObject_GetAttrString(os, (char*)"urandom");
+    PyObject *random_bits = PyObject_CallObject(urandom, key_len);
+    PyObject_GetBuffer(random_bits, &view, 0);
+    memcpy(private_key, view.buf, 32);
+    
+    private_key[ 0] &= 248;
+    private_key[31] &= 127;
+    private_key[31] |=  64;
+
+    return Py_BuildValue("y#", private_key, 32);
+}
+
+// Usage: public = curve25519.public(private) -> get public key
+static PyObject *curve25519_public(PyObject *self, PyObject *args)
+{
+    Py_buffer private;
+
+    if (PyArg_ParseTuple(args, "y*", &private))
+    {
+        uint8_t *pub_key = PyMem_Malloc(32); // public key
+        curve25519_donna(pub_key, private.buf, base_pt);
+        return Py_BuildValue("y#", pub_key, 32);
+    }
+
+    return 0;
+}
+
+// Usage: shared = curve25519.shared(your private key, his public key)
+static PyObject *curve25519_shared(PyObject *self, PyObject *args)
+{
+    Py_buffer secret;
+    Py_buffer public;
+
+    if (PyArg_ParseTuple(args, "y*y*", &secret, &public))
+    {
+        uint8_t *shared = PyMem_Malloc(32); // shared secret
+        curve25519_donna(shared, secret.buf, public.buf);
+        return Py_BuildValue("y#", shared, 32);
+    }
+
+    return 0;
+}
+
+static PyMethodDef curve25519_methods[] = 
+{
+    {"genkey",  curve25519_genkey, METH_VARARGS,
+     "Randomly generates a new private key"},
+    {"public",  curve25519_public, METH_VARARGS,
+     "Computes a public key from a private key"},
+    {"shared",  curve25519_shared, METH_VARARGS,
+     "Generates a shared secret from a key pair"},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyModuleDef curve25519_module = 
+{
+    PyModuleDef_HEAD_INIT,
+    "curve25519",
+    "Python wrapper for curve25519.",
+    -1,
+    curve25519_methods
+};
+
+PyMODINIT_FUNC PyInit_curve25519(void)
+{
+    return PyModule_Create(&curve25519_module);
+}
+
+int main(int argc, char *argv[])
+{
+    PyImport_AppendInittab("curve25519", PyInit_curve25519);
+    wchar_t progname[FILENAME_MAX + 1];
+    mbstowcs(progname, argv[0], strlen(argv[0]) + 1);
+    Py_SetProgramName(progname);
+    Py_Initialize();
+    return 0;
+}
+
+#else
+
 // Usage: private = curve25519.genkey() -> generate private key
 static PyObject *curve25519_genkey(PyObject *self, PyObject *args)
 {
@@ -79,3 +170,5 @@ int main(int argc, char *argv[])
     initcurve25519();
     return 0;
 }
+
+#endif
--- test_curve25519.py
+++ test_curve25519.py
@@ -12,26 +12,26 @@ class TestCurve25519(unittest.TestCase):
             self.assertNotEqual(private[31] & 64 , 0)   # |=  64 (x1xxxxxx)
 
     def test_public(self):
-        pri1 = "a8abababababababababababababababababababababababababababababab6b".decode("hex")
-        pri2 = "c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d".decode("hex")
+        pri1 = binascii.unhexlify("a8abababababababababababababababababababababababababababababab6b")
+        pri2 = binascii.unhexlify("c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d")
 
         pub1 = binascii.hexlify(bytearray(curve25519.public(pri1)))
         pub2 = binascii.hexlify(bytearray(curve25519.public(pri2)))
 
-        self.assertEqual(pub1, "e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859")
-        self.assertEqual(pub2, "b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f")
+        self.assertEqual(pub1, b"e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859")
+        self.assertEqual(pub2, b"b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f")
 
     def test_shared_1(self):
-        pri1 = "a8abababababababababababababababababababababababababababababab6b".decode("hex")
-        pri2 = "c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d".decode("hex")
-        pub1 = "e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859".decode("hex")
-        pub2 = "b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f".decode("hex")
+        pri1 = binascii.unhexlify("a8abababababababababababababababababababababababababababababab6b")
+        pri2 = binascii.unhexlify("c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d")
+        pub1 = binascii.unhexlify("e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859")
+        pub2 = binascii.unhexlify("b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f")
 
         shared1 = binascii.hexlify(bytearray(curve25519.shared(pri1, pub2)))
         shared2 = binascii.hexlify(bytearray(curve25519.shared(pri2, pub1)))
 
         self.assertEqual(shared1, shared2)
-        self.assertEqual(shared1, "235101b705734aae8d4c2d9d0f1baf90bbb2a8c233d831a80d43815bb47ead10")
+        self.assertEqual(shared1, b"235101b705734aae8d4c2d9d0f1baf90bbb2a8c233d831a80d43815bb47ead10")
 
     def test_shared_2(self):
         for _ in range(1024):
